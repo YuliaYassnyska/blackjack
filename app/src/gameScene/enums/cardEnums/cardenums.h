2b4e2029ad9b9fed5a6362d3bd77786420c209c3
YuliaYassnyska <yuliayasso@gmail.com>
2023-04-13 20:05:50 +0300

    Get enums for card paths

---
+++ app/src/gameScene/enums/cardEnums/cardenums.h
@@ -0,0 +1,32 @@
+#pragma once
+
+enum class Suit
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+};
+
+enum class Mode
+{
+    Light,
+    Dark
+};
+
+enum class Card
+{
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eignt,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+};
