d05af943ccb5190a13197be309890ebd8fdb465c
YuliaYassnyska <yuliayasso@gmail.com>
2023-04-17 14:05:15 +0300

    Add button items

---
+++ app/src/gameScene/items/buttonItem/buttonitem.cpp
@@ -0,0 +1,37 @@
+#include "buttonitem.h"
+
+#include <QPainter>
+
+namespace Scene
+{
+ButtonItem::ButtonItem(const QString &imagePath)
+    : _image{ QPixmap(imagePath).scaled(boundingRect().size().toSize()) }
+{
+}
+
+QRectF ButtonItem::boundingRect() const
+{
+    return QRectF(0, 0, 70, 70);
+}
+
+void ButtonItem::paint(QPainter *painter, const QStyleOptionGraphicsItem * /*option*/,
+                       QWidget *widget)
+{
+    const int penWidth{ 1 };
+    const int cornerRadius{ 50 };
+    QRect rect{ boundingRect().toRect() };
+    QRect borderRect{ QRect{ rect.x() + penWidth, rect.y() + penWidth, rect.width() - penWidth * 2,
+                             rect.height() - penWidth * 2 } };
+
+    painter->setRenderHint(QPainter::Antialiasing);
+    painter->setBackgroundMode(Qt::BGMode::OpaqueMode);
+    painter->setBackground(Qt::white);
+    painter->setBrush(_image);
+    painter->drawRoundedRect(borderRect, cornerRadius, cornerRadius);
+}
+
+void ButtonItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+}
+
+} // namespace Scene
