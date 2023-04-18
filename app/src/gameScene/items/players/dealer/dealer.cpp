eae9415e066bd6ad4d9be6b7a736448d209cd5c6
YuliaYassnyska <yuliayasso@gmail.com>
2023-04-18 23:09:12 +0300

    Delete players borders

--- app/src/gameScene/items/players/dealer/dealer.cpp
+++ app/src/gameScene/items/players/dealer/dealer.cpp
@@ -55,13 +55,5 @@ void Dealer::init()
 
 void Dealer::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    const int penWidth{ 1 };
-    const int cornerRadius{ 5 };
-    QRect rect{ boundingRect().toRect() };
-    QRect borderRect{ QRect{ rect.x() + penWidth, rect.y() + penWidth, rect.width() - penWidth * 2,
-                             rect.height() - penWidth * 2 } };
-
-    painter->setRenderHint(QPainter::Antialiasing);
-    painter->drawRoundedRect(borderRect, cornerRadius, cornerRadius);
 }
 } // namespace Scene
--- app/src/gameScene/items/players/player/player.cpp
+++ app/src/gameScene/items/players/player/player.cpp
@@ -48,14 +48,6 @@ unsigned Player::modelId() const
 
 void Player::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    const int penWidth{ 1 };
-    const int cornerRadius{ 5 };
-    QRect rect{ boundingRect().toRect() };
-    QRect borderRect{ QRect{ rect.x() + penWidth, rect.y() + penWidth, rect.width() - penWidth * 2,
-                             rect.height() - penWidth * 2 } };
-
-    painter->setRenderHint(QPainter::Antialiasing);
-    painter->drawRoundedRect(borderRect, cornerRadius, cornerRadius);
 }
 
 void Player::init()
