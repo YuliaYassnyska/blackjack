e80385a25bb35b4d6488f84a6779011160abb8c8
YuliaYassnyska <yuliayasso@gmail.com>
2023-04-18 12:21:54 +0300

    Change minimum window size

--- app/src/mainwindow.cpp
+++ app/src/mainwindow.cpp
@@ -29,7 +29,7 @@ MainWindow::~MainWindow()
 void MainWindow::initWindow()
 {
     setWindowTitle(QStringLiteral("blackjack"));
-    setMinimumSize(750, 500);
+    setMinimumSize(950, 600);
     _view->setScene(_scene);
     setCentralWidget(_view);
     _scene->setSceneRect(rect());
